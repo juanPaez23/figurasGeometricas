34feaf2a6fc05b19c1b3e91828d0bd8069502d85
juanPaez23 <jpaezbeltran2015@gmail.com>
2021-03-17 13:55:31 -0500

    Creacion de paquetes

---
+++ src/main/java/co/edu/ucundinamarca/controlador/Aplicacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.controlador;
+
+
+
+
+
+/**
+ * Clase encargada de realizar la ejecución del programa.
+ * @author Eison Andrei Morales Pardo
+ * @author Juan Camilo Páez Beltran 
+ * @since 1.0
+ * @version 1.0.5
+ */
+public class Aplicacion {
+    /**
+     * Método encargado de ejecutar el programa
+     * @param args 
+     */
+    public static void main(String[] args) {
+      
+      
+    }}
