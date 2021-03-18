e5e0929d629b28029a5a3b0b94cc5fe08f11e007
juanPaez23 <jpaezbeltran2015@gmail.com>
2021-03-18 12:30:47 -0500

    Agregado de métodos y atributos a la clase padre

--- src/main/java/co/edu/ucundinamarca/modelo/figurasPlanas.java
+++ src/main/java/co/edu/ucundinamarca/modelo/figurasPlanas.java
@@ -6,9 +6,49 @@
 package co.edu.ucundinamarca.modelo;
 
 /**
- *
- * @author jpaez
+ *Clase padre encargada de realizar operaciones sobre figuras geometricas planas
+ *tales como hallar su perímetro y su área.
+ * 
+ * @author Juan Camilo Paez Beltran
+ * @author Eison Andrei Morales Pardo
+ * @since 1.0
+ * @version 1.0.0
  */
+
 public class figurasPlanas {
     
+    /**
+     * Variable encargada de alojar el valor del perimetro de la figura geometrica
+     */
+    float perimetro;
+    /**
+     * Variable encargada de alojar el valor del area de la figura geometrica
+     */
+    float area;
+    /**
+     * Variable encargada de alojar el numero de lados de la figura
+     */
+    byte numeroLados;
+    
+    
+    
+    /**
+     * Metodo encargado de capturar las medidas de las figuras geométricas,
+     * calcular y retornar el valor de su perímetro
+     * @return El perimetro calculado
+     */
+    public float calcularPerimetro (){
+    
+    return perimetro;
+    }
+    
+    /**
+     * Metodo encargado de capturar las medidas de las figuras geométricas,
+     * calcular y retornar el valor de su perímetro
+     * @return El area calculada
+     */
+    public float calcularArea (){
+    
+    return area;
+    }
 }
